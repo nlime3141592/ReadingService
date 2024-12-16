efd821b3bf320e90871eea979eb3107625d2e3ab
DESKTOP-6B7URQN\deu <dot0112@naver.com>
2024-12-16 22:26:38 +0900

    feat: change HTML2NotionStyle parser - now support NotionAPI

--- WebServer/static/js/fr_bookReport.js
+++ WebServer/static/js/fr_bookReport.js
@@ -57,70 +57,127 @@ function test() {
 }
 
 /**
- * richTextEditor의 전체 HTML 코드를 Notion 형태로 변경
- * @param {HTMLObjectElement} element richTextEditor 1단계 하위의 HTML 코드들
- * @returns 변경된 string 반환 - 그대로 Notion에 넣으면 됨
+ * Rich Text에 작성된 텍스트의 HTML을 Notion api의 형태에 맞추어 변환함 - api 키는 설정하지않음
+ * @param {HTMLObjectElement} element Rich Text에 작성된 HTML
+ * @returns Notion api에 입력될 body의 JSON
  */
 function htmlToNotion(element) {
   let children = element.children;
-  let result = [];
+  let result = {
+    block_id: "",
+    children: [],
+  };
 
   for (let i = 0; i < children.length; i++) {
     let child = children[i];
     if (child.tagName.toLowerCase() != "ol") {
-      let text = child.outerHTML;
-      result.push(simpleTagChange(text));
+      richText = lineParser(child);
+      result["children"].push(richText);
     } else {
-      result.push(listTagChange(child));
+      richText = listParser(child);
+      result["children"].push(...richText);
     }
   }
-
-  return result.join("");
+  return result;
 }
 
 /**
- * 리스트 태그의 변환
- * @param {HTMLObjectElement} olTag 변환할 ol 태그
- * @returns 변환된 ol 태그의 string
+ * 한 줄로 끝나는 태그를 변환
+ * @param {HTMLObjectElement} text <ol>이 아닌 태그들 - 한 줄로 종료되는
+ * @returns 변환된 JSON 오브젝트
  */
-function listTagChange(olTag) {
-  let numbering = 1;
-  let children = olTag.children;
+function lineParser(text) {
+  const nameDict = {
+    p: "paragraph",
+    h1: "heading_1",
+    h2: "heading_2",
+    h3: "heading_3",
+  };
+
+  const tagName = text.tagName.toLowerCase();
+  const blockType = nameDict[tagName];
+
+  if (!blockType) {
+    throw new Error(`Unsupported tag name: ${tagName}`);
+  }
+
+  return {
+    object: "block",
+    type: blockType,
+    [blockType]: { rich_text: recursiveParser(text) },
+  };
+}
+
+/**
+ * 한 줄로 끝나지 않는 태그를 반환
+ * @param {HTMLObjectElement} text <ol> 태그 - 여러 줄이 자식 요소
+ * @returns 변환된 JSON 오브젝트
+ */
+function listParser(text) {
+  const nameDict = {
+    ordered: "numbered_list_item",
+    bullet: "bulleted_list_item",
+  };
+  const children = text.children;
   let result = [];
   for (let i = 0; i < children.length; i++) {
-    let child = children[i];
-    let listData = child.dataset.list;
-    let text = simpleTagChange(child.innerHTML) + "\n";
-    text = (listData == "ordered" ? `${numbering++}. ` : `* `) + text;
-    result.push(text);
+    const child = children[i];
+    const type = child.dataset.list;
+    const typeValue = nameDict[type];
+    result.push({
+      object: "block",
+      type: typeValue,
+      [typeValue]: {
+        rich_text: recursiveParser(child),
+      },
+    });
   }
-  return result.join("");
+  return result;
 }
 
 /**
- * 태그를 직접 Notion 스타일로 변경
- * @param {HTMLObjectElement} text 변경할 태그 - <p></p> 단위로 받음, <br>은 예외
- * @returns 변환된 태그의 string
+ * 자식 노드들까지 반복하며 스타일을 부여함
+ * @param {HTMLObjectElement} text 변환할 HTML 코드
+ * @param {string} option 스타일을 string의 형태로 나타냄 - 'b':bold, 'u':underline, 'i':italic
+ * @returns 변환된 JSON 오브젝트
  */
-function simpleTagChange(text) {
-  // delete span
-  text = text.replace(/<span[^>]*>.*?<\/span>/gs, "");
-  // bold
-  text = text.replace(/<strong>/g, "**").replace(/<\/strong>/g, "**");
-  // italic
-  text = text.replace(/<em>/g, "_").replace(/<\/em>/g, "_");
-  // h1
-  text = text.replace(/<h1>/g, "# ").replace(/<\/h1>/g, "\n");
-  // h2
-  text = text.replace(/<h2>/g, "## ").replace(/<\/h2>/g, "\n");
-  // h3
-  text = text.replace(/<h3>/g, "### ").replace(/<\/h3>/g, "\n");
-  // <p>
-  text = text.replace(/<p>/g, "").replace(/<\/p>/g, "\n");
-  // <u>
-  text = text.replace(/<u>/g, "").replace(/<\/u>/g, "");
-  // <br>
-  text = text.replace(/<br>/g, "\n");
-
-  return text;
+function recursiveParser(text, option = "") {
+  let richText = [];
+  const childNodes = text.childNodes;
+  for (let i = 0; i < childNodes.length; i++) {
+    if (childNodes[i].nodeType != Node.TEXT_NODE) {
+      const tagName = childNodes[i].tagName.toLowerCase();
+      let additionalOption = "";
+      if (tagName === "br") {
+        // 줄바꿈을 나타내는 빈 텍스트 조각 추가
+        richText.push({ text: { content: "" }, annotations: {} });
+        continue; // 다음 노드로 넘어갑니다.
+      }
+      switch (tagName) {
+        case "strong":
+          additionalOption = "b";
+          break;
+        case "u":
+          additionalOption = "u";
+          break;
+        case "em":
+          additionalOption = "i";
+          break;
+      }
+      richText.push(
+        ...recursiveParser(childNodes[i], option + additionalOption)
+      );
+    } else {
+      let annotationed = {
+        text: { content: childNodes[i].textContent },
+        annotations: {
+          bold: option.includes("b"),
+          underline: option.includes("u"),
+          italic: option.includes("i"),
+        },
+      };
+      richText.push(annotationed);
+    }
+  }
+  return richText;
 }
