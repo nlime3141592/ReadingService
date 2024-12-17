c2fdbe6203ff90ccdbc28ccaaaf366c54f3c1cf3
limeLimelimeLime <nlime3141592@naver.com>
2024-12-17 21:16:43 +0900

    fix: https 서버로 변경

--- WebServer/main.js
+++ WebServer/main.js
@@ -1,6 +1,7 @@
 // NOTE: 외부 모듈을 임포트합니다.
 const express = require("express");
-const fs = require("fs").promises;
+const https = require("https");
+const fs = require("fs");
 
 // NOTE: 본 프로젝트에서 구현한 모듈(소스 코드)을 임포트합니다.
 const utility = require("./src/utility.js");
@@ -16,6 +17,7 @@ const api_readnote = require("./src/api/api_readnote.js");
 // NOTE: 상수
 const c_NUM_PORT = 8080;
 
+// NOTE: https 서버 인증을 위한 키
 const privateKey = fs.readFileSync("C:\\Programming\\web\\ssl\\private-key.pem", "utf8")
 const certificate = fs.readFileSync("C:\\Programming\\web\\ssl\\certificate.pem", "utf8")
 const credentials = { key: privateKey, cert: certificate }
@@ -27,7 +29,7 @@ async function main()
 {
   app.use(express.json());
   app.use(express.static(utility.getStaticDirectory()));
-    
+
   // TODO: 이 곳에서 사용할 API 모듈을 초기화(init)합니다.
   api_main.init(app);
   api_test.init(app); // TODO: 배포 시 이 줄은 주석 처리하세요.
@@ -36,10 +38,16 @@ async function main()
 
   await db_connection.init()
 
-  // NOTE: 서버를 열고 클라이언트 요청 발생을 대기합니다.
+  // NOTE: 서버를 열고 클라이언트 요청 발생을 대기합니다. (https 서버)
+  const server = https.createServer(credentials, app).listen(c_NUM_PORT, () => {
+    utility.printLogWithName(`서버를 시작합니다. (http://localhost:${c_NUM_PORT})`, "System");
+  });
+  /*
+  // NOTE: 서버를 열고 클라이언트 요청 발생을 대기합니다. (http 서버)
   const server = app.listen(c_NUM_PORT, () => {
     utility.printLogWithName(`서버를 시작합니다. (http://localhost:${c_NUM_PORT})`, "System");
   });
+  */
 
   // NOTE:
   // CTRL+C 입력 시 발생하는 SIGINT signal에 대한 이벤트 핸들러를 등록합니다.
