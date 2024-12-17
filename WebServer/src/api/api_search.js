70d0bf99d713eb656dae81613ed9a36d59b8eabe
limeLimelimeLime <nlime3141592@naver.com>
2024-12-17 20:14:53 +0900

    fix: 검색 URI 수정, DB 연결 성공, python 연결 성공

--- WebServer/src/api/api_search.js
+++ WebServer/src/api/api_search.js
@@ -5,22 +5,20 @@ module.exports = {
 const utility = require("../utility.js")
 const dbQuery = require("../db/db_query.js")
 
-const BOOKS_PER_PAGE = 10
-
 function __init(app)
 {
-    app.get("/search/all/:pageNum", __get_search_all)
-    app.get("/search/by-isbn13/:isbn13/:pageNum", __get_search_by_isbn13)
-    app.get("/search/by-keyword/:keyword/:pageNum", __get_search_by_keyword)
-    app.get("/search/by-recommendation/:pageNum", __get_search_by_recommendation)
-    app.get("/search/by-history/:pageNum", __get_search_by_history)
+    app.get("/search/all", __get_search_all)
+    app.get("/search/by-isbn13/:isbn13", __get_search_by_isbn13)
+    app.get("/search/by-keyword/:keyword", __get_search_by_keyword)
+    app.get("/search/by-recommendation", __get_search_by_recommendation)
+    app.get("/search/by-history", __get_search_by_history)
 }
 
 function __get_search_all(req, res)
 {
-    let { pageNum } = req.params
+    let { pageNum, booksPerPage } = req.query
 
-    jsonPages = dbQuery.query_page_from_all_books(pageNum, BOOKS_PER_PAGE)
+    jsonPages = dbQuery.query_page_from_all_books(pageNum, booksPerPage)
     jsonPages = {
         "test-json": "please-this-json-remove-after-test"
     }
@@ -31,9 +29,10 @@ function __get_search_all(req, res)
 
 function __get_search_by_isbn13(req, res)
 {
-    let { isbn13, pageNum } = req.params
+    let { isbn13 } = req.params
+    let { pageNum, booksPerPage } = req.query
 
-    jsonPages = dbQuery.query_page_from_isbn13(isbn13, pageNum, BOOKS_PER_PAGE)
+    jsonPages = dbQuery.query_page_from_isbn13(isbn13, pageNum, booksPerPage)
     jsonPages = {
         "test-json": "please-this-json-remove-after-test"
     }
@@ -44,9 +43,10 @@ function __get_search_by_isbn13(req, res)
 
 function __get_search_by_keyword(req, res)
 {
-    let { keyword, pageNum } = req.params
+    let { keyword } = req.params
+    let { pageNum, booksPerPage } = req.query
 
-    jsonPages = dbQuery.query_page_from_keyword(keyword, pageNum, BOOKS_PER_PAGE)
+    jsonPages = dbQuery.query_page_from_keyword(keyword, pageNum, booksPerPage)
     jsonPages = {
         "test-json": "please-this-json-remove-after-test"
     }
@@ -55,22 +55,26 @@ function __get_search_by_keyword(req, res)
     utility.printLog("검색 요청 처리 완료 (키워드 검색)")
 }
 
-function __get_search_by_recommendation(req, res)
+async function __get_search_by_recommendation(req, res)
 {
-    let { pageNum } = req.params
-
     jsonPages = {
         "test-json": "please-this-json-remove-after-test"
     }
     res.send(JSON.stringify(jsonPages))
 
-    utility.printLog("검색 요청 처리 완료 (추천 도서)")
+    let modulePath = utility.getPythonPath("api_recommendation.py")
+
+    let command = `python ${modulePath}`
+    let execSuccess = await utility.execPromise(command)
+
+    if (execSuccess === true)
+        utility.printLog("검색 요청 처리 완료 (추천 도서)")
+    else
+        utility.printLog("검색 요청 처리 실패 (추천 도서)")
 }
 
 function __get_search_by_history(req, res)
 {
-    let { pageNum } = req.params
-
     jsonPages = {
         "test-json": "please-this-json-remove-after-test"
     }
--- WebServer/src/db/db_query.js
+++ WebServer/src/db/db_query.js
@@ -4,9 +4,36 @@ module.exports = {
     query_page_from_keyword: __query_page_from_keyword
 }
 
-function __query_page_from_all_books(pageNum, booksPerPage)
+const utility = require("../utility.js")
+const db_connection = require("./db_connection.js")
+
+async function __query_page_from_all_books(pageNum, booksPerPage)
 {
-    
+    const dbBook = db_connection.get_db("book")
+    const sourceCollection = dbBook.collection("bookkeywords")
+
+    booksPerPage = Number(booksPerPage)
+    let skipBookNum = pageNum * booksPerPage
+
+    const query = { weight: 25 }
+    const batch = await sourceCollection
+        .find(query)
+        // .sort({ _id: 1 })
+        .skip(skipBookNum)
+        .limit(booksPerPage)
+        .toArray()
+
+    if (batch.length === 0) 
+    {
+        utility.printLogWithName("No data gets.", "dbQuery")
+    }
+    else
+    {
+        for (let result of batch)
+        {
+            utility.printLogWithName(JSON.stringify(result), "dbQuery - TEST")
+        }
+    }
 }
 
 function __query_page_from_isbn13(isbn13, pageNum, booksPerPage)
